58c6160f84eb3b9e6232bec48ccd75fd87b8fbb9
Rick Riehle <rickr@bitbamboo.com>
2018-02-13 07:26:39 -0700

    Push and pop working git branch

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+working_git_branch="$(git rev-parse --abbrev-ref HEAD)"
 git checkout gh-pages
 rm -rf build/*
-git merge --commit --no-edit master
+git merge --commit --no-edit $working_git_branch
 touch .nojekyll  # Make sure the repo has this file in its root, otherwise it will not render on github.io
 make html
 open build/html/index.html  # On OS X this launches the rendered page into a browser; need something else for Linux or Windows
@@ -10,3 +10,4 @@ git add *
 git commit -a --no-edit -m "Updating presentation materials"
 git pull -s ours --no-edit
 git push
+git checkout $working_git_branch
