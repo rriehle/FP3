f8beef518b77ab5b62e3cf174c1c80c0d9823e81
Rick Riehle <rriehle@uw.edu>
2018-02-19 13:42:37 -0700

    Authors

--- source/conf.py
+++ source/conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'Lesson Plan'
 
-author = 'Rick Riehle'
+author = "Brian Dorsey, Christopher Barker, Christy Heaton, Cris Ewing, Jon Jacky, Joseph Schilz, Maria McKinley, Rick Riehle"
 
 copyright = (
     "{}, University of Washington, {}. Creative Commons Attribution-ShareAlike 3.0 license".format(
